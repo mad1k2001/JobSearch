de6342148a363a7ffd45276d63999212c79f56f0
Madina Beishenalieva <madinakg2003@gmail.com>
2024-03-07 20:12:40 +0600

    create WorkExperienceinfo

---
+++ src/main/java/com/example/jobsearch/model/WorkExperienceInfo.java
@@ -0,0 +1,11 @@
+package com.example.jobsearch.model;
+
+public class WorkExperienceInfo {
+    private Long id;
+    private Integer resumeId;
+    private Integer years;
+    private String companyName;
+    private String position;
+    private String responsibility;
+}
+
