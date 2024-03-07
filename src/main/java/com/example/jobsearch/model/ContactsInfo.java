8902bf2e0033da30d23a86c6321f220cafb0ebb1
Madina Beishenalieva <madinakg2003@gmail.com>
2024-03-07 19:01:46 +0600

    create model Resumes and ContactsInfo

---
+++ src/main/java/com/example/jobsearch/model/ContactsInfo.java
@@ -0,0 +1,8 @@
+package com.example.jobsearch.model;
+
+public class ContactsInfo {
+    private Long id;
+    private Integer typeId;
+    private Integer resumeId;
+    private String value;
+}
---
+++ src/main/java/com/example/jobsearch/model/Resumes.java
@@ -0,0 +1,14 @@
+package com.example.jobsearch.model;
+
+import java.time.LocalDate;
+
+public class Resumes {
+    private Long id;
+    private Integer applicantId;
+    private String name;
+    private Integer categoryId;
+    private Double salary;
+    private Boolean isActive;
+    private LocalDate createdDate;
+    private LocalDate updateTime;
+}
