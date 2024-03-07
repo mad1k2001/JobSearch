1a10af57dd80d29c8ab709e697800388141faf45
Madina Beishenalieva <madinakg2003@gmail.com>
2024-03-07 18:33:49 +0600

    create model User and Vacancies

---
+++ src/main/java/com/example/jobsearch/model/User.java
@@ -0,0 +1,13 @@
+package com.example.jobsearch.model;
+
+
+public class User {
+    private Long id;
+    private String name;
+    private String surname;
+    private Integer age;
+    private Integer password;
+    private Integer phoneNumber;
+    private String avatar;
+    private String accountType;
+}
---
+++ src/main/java/com/example/jobsearch/model/Vacancies.java
@@ -0,0 +1,18 @@
+package com.example.jobsearch.model;
+
+import java.time.LocalDate;
+
+public class Vacancies {
+    private Long id;
+    private String name;
+    private String description;
+    private Integer categoryId;
+    private Integer salary;
+    private Integer expFrom;
+    private Integer expTo;
+    private Boolean isActivate;
+    private Integer authorId;
+    private LocalDate createdDate;
+    private LocalDate updateTime;
+
+}
