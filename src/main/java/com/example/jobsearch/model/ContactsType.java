d5cd30cfa1f051d9325964c3c41ffe16cb520613
Madina Beishenalieva <madinakg2003@gmail.com>
2024-03-07 19:07:08 +0600

    create model ContactInfo and EducationInfo

---
+++ src/main/java/com/example/jobsearch/model/ContactsType.java
@@ -0,0 +1,8 @@
+package com.example.jobsearch.model;
+
+public class ContactsType {
+    private Long id;
+    private Integer typeId;
+    private Integer resumeId;
+    private String value;
+}
---
+++ src/main/java/com/example/jobsearch/model/EducationInfo.java
@@ -0,0 +1,14 @@
+package com.example.jobsearch.model;
+
+
+import java.time.LocalDate;
+
+public class EducationInfo {
+    private Long id;
+    private Integer resumeId;
+    private String institution;
+    private String program;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String degree;
+}
